e5c84cba42ee38a6ee13c36c932fd7b671ad884e
sambagga <sam.bagga@gmail.com>
2013-02-19 02:09:06 -0500

    Initial Commit, Group Messenger UI provided

---
+++ src/edu/buffalo/cse/cse486586/groupmessenger/GroupMessengerActivity.java
@@ -0,0 +1,28 @@
+package edu.buffalo.cse.cse486586.groupmessenger;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class GroupMessengerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_messenger);
+
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+        findViewById(R.id.button1).setOnClickListener(
+                new OnPTestClickListener(tv, getContentResolver()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
+        return true;
+    }
+}
